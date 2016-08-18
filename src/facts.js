a62eaf121733761c796c639ec36a86a9dd333b1f
jonathanbyrne <jonathan.byrne@yahoo.com>
2016-08-17 20:00:41 -0400

    Added support for multiple tokens and fixed bugs with facts I added earlier

--- src/facts.js
+++ src/facts.js
@@ -4,34 +4,64 @@ var local = YAML.load('local-facts.yml');
 
 function randomRange(low, high)
 {
-	return Math.floor(Math.random() * (high - low) + low);
+    return Math.floor(Math.random() * (high - low) + low);
 }
 
 function randomElement(array)
 {
-	var index = Math.floor(Math.random() * array.length);
-	return array[index];
+    var index = Math.floor(Math.random() * array.length);
+    return array[index];
 }
 
 module.exports = Facts;
 function Facts()
 {
-	for (var type in local)
-	{
-		data[type] = data[type].concat(local[type]);
-	}
+    for (var type in local)
+    {
+        if (type in data)
+        {
+            data[type] = data[type].concat(local[type]);
+        }
+        else
+        {
+            data[type] = local[type];
+        }
+    }
 }
 
-Facts.prototype.generateFact = function()
+Facts.prototype.generateFact = function ()
 {
-	var text = randomElement(data.text);
-	for (var type in data)
-	{
-		if (type == 'text') continue;
-		var typeRe = new RegExp("\{" + type + "\}", "g");	
-		text = text.replace(typeRe, randomElement(data[type]));
-	}
-	var re = new RegExp("\{oldyear\}");
-	text = text.replace(re, randomRange(1960,1990));
-	return text;
+    var text = randomElement(data.text);
+    var changed = true;
+    var usedItems = [];
+    while (changed)
+    {
+        changed = false;
+        for (var type in data)
+        {
+            if (type == 'text')
+            {
+                continue;
+            }
+            while (text.search("\{" + type + "\}") >= 0)
+            {
+                var replacement;
+                var checks = 0;
+                do
+                {
+                    replacement = randomElement(data[type]);
+                    checks++;
+                }
+                while (replacement in usedItems || checks > 20);
+                usedItems.push(replacement);
+                text = text.replace("\{" + type + "\}", replacement);
+                changed = true;
+            }
+        }
+    }
+    var re = new RegExp("\{oldyear\}");
+    text = text.replace(re, randomRange(1960, 1990));
+    text = text.substring(0, 1).toUpperCase() + text.substring(1, text.length);
+
+    return text;
 };
